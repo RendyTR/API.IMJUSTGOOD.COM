b24c261b1a9fb08a300060c7d13d3fc043a04e4d
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 13:29:00 +0700

    .

--- example/youtube_downloader.py
+++ example/youtube_downloader.py
@@ -1,17 +1,20 @@
 from justgood import imjustgood
 
-api     = imjustgood("INSERT_YOUR_APIKEY_HERE")
-data    = api.youtubedl("https://youtu.be/POG1xVW-1jQ")
+api      =  imjustgood("INSERT_YOUR_APIKEY_HERE")
+data     =  api.youtubedl("https://youtu.be/POG1xVW-1jQ")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
-result  = "Title : {}".format(data["result"]["title"])
-result += "\nAuthor : {}".format(data["result"]["author"])
-result += "\nDuratiOn : {}".format(data["result"]["duration"])
-result += "\nWatch : {}\n".format(data["result"]["watched"])
-result += "\nDesc :\n{}\n".format(data["result"]["description"])
-result += "\nThumb :\n{}\n".format(data["result"]["thumbnail"])
-result += "\nAudio :\n{}\n".format(data["result"]["audioUrl"])
-result += "\nVideo :\n{}\n".format(data["result"]["videoUrl"])
-result += "\nPage : {}".format(data["result"]["pageUrl"])
-print(result)
+
+thumb    =  data["result"]["thumbnail"]
+video    =  data["result"]["audioUrl"]
+audio    =  data["result"]["videourl"]
+
+result   =  "Title : {}\n".format(data["result"]["title"])
+result  +=  "Author : {}\n".format(data["result"]["author"])
+result  +=  "Duration : {}\n".format(data["result"]["duration"])
+result  +=  "Watched : {}\n\n".format(data["result"]["watched"])
+result  +=  "Description :\n{}\n\n".format(data["result"]["description"])
+result  +=  "Page URL : {}".format(data["result"]["pageUrl"])
+
+print(thumb, result, audio, video)
