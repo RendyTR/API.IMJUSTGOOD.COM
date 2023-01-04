82518a9bb4f224230b10fe2c01e873625a9eed76
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 13:58:14 +0700

    .

--- example/joox_music.py
+++ example/joox_music.py
@@ -1,15 +1,17 @@
 from justgood import imjustgood
 
-api     = imjustgood("INSERT_YOUR_APIKEY_HERE")
-data    = api.joox("lathi")
+api      =  imjustgood("INSERT_YOUR_APIKEY_HERE")
+data     =  api.joox("lathi")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
-result  = "Singer : {}".format(data["result"]["singer"])
-result += "\nTitle : {}".format(data["result"]["title"])
-result += "\nDuration : {}".format(data["result"]["duration"])
-result += "\nSize : {}".format(data["result"]["size"])
-result += "\n\nThumbnail :\n{}".format(data["result"]["thumbnail"])
-result += "\n\nM4a :\n{}".format(data["result"]["m4aUrl"])
-result += "\n\nMp3 :\n{}".format(data["result"]["mp3Url"])
-print(result)
+
+thumb    =  data["result"]["thumbnail"]
+audio    =  data["result"]["m4aUrl"]
+
+result   =  "Title : {}\n".format(data["result"]["title"])
+result  +=  "Singer : {}\n".format(data["result"]["singer"])
+result  +=  "Duration : {}\n".format(data["result"]["duration"])
+result  +=  "File size : {}".format(data["result"]["size"])
+
+print(thumb, result, audio)
