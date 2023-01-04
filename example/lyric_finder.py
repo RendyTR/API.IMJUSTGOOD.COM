f3850537aeee51ef8372bbd4cf280dd429c97364
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 14:06:19 +0700

    .

--- example/lyric_finder.py
+++ example/lyric_finder.py
@@ -7,9 +7,7 @@ print(data)
 # EXAMPLE GET CERTAIN ATTRIBUTES
 
 image    =  data["result"]["image"]
-
 result  +=  "Title : {}\n".format(data["result"]["title"])
 result  +=  "Artist : {}\n".format(data["result"]["artist"])
 result  +=  "Lyric :\n\n{}".format(data["result"]["lyric"])
-
 print(image, result)
