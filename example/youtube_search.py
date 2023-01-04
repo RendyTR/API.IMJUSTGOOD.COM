4253c6d0ef4a1b55e81e7e2cd0e05c2c2c1197fe
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 07:54:35 +0700

    .

--- example/youtube_search.py
+++ example/youtube_search.py
@@ -1,6 +1,6 @@
 from justgood import imjustgood
 
-api     = imjustgood("YOUR_APIKEY_HERE")
+api     = imjustgood("INSERT_YOUR_APIKEY_HERE")
 data    = api.youtube("friends")
 print(data)
 
@@ -8,10 +8,10 @@ print(data)
 result  = "Title : {}".format(data["result"]["title"])
 result += "\nAuthor : {}".format(data["result"]["author"])
 result += "\nDuration : {}".format(data["result"]["duration"])
-result += "\nWatched : {}\n".format(data["result"]["watched"])
-result += "\nDescription :\n{}\n".format(data["result"]["watched"])
-result += "\nThumbnail :\n{}\n".format(data["result"]["thumbnail"])
+result += "\nWatc : {}\n".format(data["result"]["watched"])
+result += "\nDesc :\n{}\n".format(data["result"]["description"])
+result += "\nThumb :\n{}\n".format(data["result"]["thumbnail"])
 result += "\nAudio :\n{}\n".format(data["result"]["audioUrl"])
-result += "\n\nVideo :\n{}".format(data["result"]["videoUrl"])
-result += "\n\nPage : {}".format(data["result"]["pageUrl"])
+result += "\nVideo :\n{}\n".format(data["result"]["videoUrl"])
+result += "\nPage : {}".format(data["result"]["pageUrl"])
 print(result)
