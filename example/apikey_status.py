7edfc4be7ee917b6e0273435d34f2157d302ee5c
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 13:59:14 +0700

    .

--- example/apikey_status.py
+++ example/apikey_status.py
@@ -5,10 +5,12 @@ data     =  api.status()
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
+
 result   =  "ID : {}\n".format(data["result"]["id"])
 result  +=  "Type : {}\n".format(data["result"]["type"])
 result  +=  "Usage : {}\n".format(data["result"]["usage"])
 result  +=  "Expired : {}\n".format(data["result"]["expired"])
 result  +=  "Restart : {}\n".format(data["result"]["restart"])
 result  +=  "Timeleft : {}".format(data["result"]["timeleft"])
+
 print(result)
