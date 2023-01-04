7f617836adf45c8f9aba2cd27837faf80a87a677
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 07:49:14 +0700

    .

--- example/youtube_search.py
+++ example/youtube_search.py
@@ -1,16 +1,17 @@
 from justgood import imjustgood
 
 api     = imjustgood("YOUR_APIKEY_HERE")
-data    = api.youtube("despacito")
+data    = api.youtube("friends")
 print(data)
 
 # EXAMPLE GET CERTAIN ATTRIBUTES
 result  = "Title : {}".format(data["result"]["title"])
 result += "\nAuthor : {}".format(data["result"]["author"])
 result += "\nDuration : {}".format(data["result"]["duration"])
-result += "\nWatched : {}".format(data["result"]["watched"])
-result += "\n\nThumbnail :\n{}".format(data["result"]["thumbnail"])
-result += "\n\nAudio :\n{}".format(data["result"]["audioUrl"])
+result += "\nWatched : {}\n".format(data["result"]["watched"])
+result += "\nDescription :\n{}\n".format(data["result"]["watched"])
+result += "\nThumbnail :\n{}\n".format(data["result"]["thumbnail"])
+result += "\nAudio :\n{}\n".format(data["result"]["audioUrl"])
 result += "\n\nVideo :\n{}".format(data["result"]["videoUrl"])
 result += "\n\nPage : {}".format(data["result"]["pageUrl"])
 print(result)
