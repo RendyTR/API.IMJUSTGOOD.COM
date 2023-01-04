f0cc8b839beae53ae23cfb5bd821100cf53c543a
RendyTR <77181144+RendyTR@users.noreply.github.com>
2023-01-04 07:53:01 +0700

    .

---
+++ example/youtube_downloader.py
@@ -0,0 +1,17 @@
+from justgood import imjustgood
+
+api     = imjustgood("YOUR_APIKEY_HERE")
+data    = api.youtubedl("https://youtu.be/POG1xVW-1jQ")
+print(data)
+
+# EXAMPLE GET CERTAIN ATTRIBUTES
+result  = "Title : {}".format(data["result"]["title"])
+result += "\nAuthor : {}".format(data["result"]["author"])
+result += "\nDuration : {}".format(data["result"]["duration"])
+result += "\nWatch : {}\n".format(data["result"]["watched"])
+result += "\nDesc :\n{}\n".format(data["result"]["description"])
+result += "\nThumb :\n{}\n".format(data["result"]["thumbnail"])
+result += "\nAudio :\n{}\n".format(data["result"]["audioUrl"])
+result += "\nVideo :\n{}\n".format(data["result"]["videoUrl"])
+result += "\nPage : {}".format(data["result"]["pageUrl"])
+print(result)
